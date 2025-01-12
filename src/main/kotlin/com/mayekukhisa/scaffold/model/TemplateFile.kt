e3a291520e060883281639b993dbb2a4c1e1bb9b
Mayeku Khisa <101919651+mayekukhisa@users.noreply.github.com>
2025-01-12 14:06:22 +0300

    chore: update copyright notice year

--- src/main/kotlin/com/mayekukhisa/scaffold/model/TemplateFile.kt
+++ src/main/kotlin/com/mayekukhisa/scaffold/model/TemplateFile.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Mayeku Khisa.
+ * Copyright 2024-2025 Mayeku Khisa.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
